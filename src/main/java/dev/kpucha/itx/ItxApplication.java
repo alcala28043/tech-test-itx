4f18f9c6a4800bde19c8714d0f83f9abc16e784b
Arturo Alcala <alcala28043@gmail.com>
2023-05-04 21:23:35 +0200

    first commit

---
+++ src/main/java/dev/kpucha/itx/ItxApplication.java
@@ -0,0 +1,13 @@
+package dev.kpucha.itx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItxApplication.class, args);
+	}
+
+}
