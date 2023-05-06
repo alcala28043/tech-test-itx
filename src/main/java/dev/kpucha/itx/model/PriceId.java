87d0310637fed5561fa08ec6952a1cbc941ef64e
Arturo Alcala <alcala28043@gmail.com>
2023-05-06 18:40:36 +0200

    FIX - Forgotten no-argument constructor

--- src/main/java/dev/kpucha/itx/model/PriceId.java
+++ src/main/java/dev/kpucha/itx/model/PriceId.java
@@ -8,11 +8,13 @@ import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @Embeddable
 @EqualsAndHashCode
 @AllArgsConstructor
+@NoArgsConstructor
 public class PriceId implements Serializable {
 	/**
 	 * 
