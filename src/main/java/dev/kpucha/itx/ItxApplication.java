e72d1657780c6297abc579a726d811d92197a164
Arturo Alcala <alcala28043@gmail.com>
2023-05-06 23:35:47 +0200

    CFG - Enable swagger and fix pom for maven builds

--- src/main/java/dev/kpucha/itx/ItxApplication.java
+++ src/main/java/dev/kpucha/itx/ItxApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
 @EnableJpaRepositories
 @EntityScan
+@OpenAPIDefinition
 public class ItxApplication {
 
 	public static void main(String[] args) {
