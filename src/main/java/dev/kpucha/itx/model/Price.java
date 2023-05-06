548e6db163e3dc444606bf81dc6bab3ccb28250f
Arturo Alcala <alcala28043@gmail.com>
2023-05-06 13:02:13 +0200

    Change to embeddedId to avoid creating new columns

--- src/main/java/dev/kpucha/itx/model/Price.java
+++ src/main/java/dev/kpucha/itx/model/Price.java
@@ -4,10 +4,8 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,22 +17,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Price {
+	
+	@EmbeddedId
+	private PriceId id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(name = "BRAND_ID")
-    private Integer brandId;
-    @Column(name = "START_DATE")
-    private LocalDateTime startDate;
-    @Column(name = "END_DATE")
-    private LocalDateTime endDate;
     @Column(name = "PRICE_LIST")
     private Integer priceList;
-    @Column(name = "PRODUCT_ID")
-    private Integer productId;
-    @Column(name = "PRIORITY")
-    private Integer priority;
     @Column(name = "PRICE")
     private BigDecimal price;
     @Column(name = "CURR")
---
+++ src/main/java/dev/kpucha/itx/model/PriceId.java
@@ -0,0 +1,33 @@
+package dev.kpucha.itx.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Embeddable
+@EqualsAndHashCode
+@AllArgsConstructor
+public class PriceId implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -18330763756867262L;	
+
+    @Column(name = "PRODUCT_ID")
+    private Integer productId;
+    @Column(name = "BRAND_ID")
+    private Integer brandId;
+    @Column(name = "START_DATE")
+    private LocalDateTime startDate;
+    @Column(name = "END_DATE")
+    private LocalDateTime endDate;
+    @Column(name = "PRIORITY")
+    private Integer priority;
+
+}
